018114c0fba33609151bebdaaca88d6d73a93837
aakwewaanaqa <vvbdac58@gmail.com>
2023-06-09 11:19:41 +0800

    ...

---
+++ Assets/InEditor/Monobehaviour/Test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InEditor
+{
+    [AddComponentMenu("InEditor/Test")]
+    public class Test : MonoBehaviour
+    {
+    }
+}
