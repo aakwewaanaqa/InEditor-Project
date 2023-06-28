932a88289e4f6456a2739d16761083a3f9fee273
aakwewaanaqa <vvbdac58@gmail.com>
2023-06-29 00:52:50 +0800

    Found that uninitialized Gradient will crash Unity

--- Runtime/Monobehaviour/NotSerializedTest.cs
+++ Runtime/Monobehaviour/NotSerializedTest.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization;
 using UnityEngine;
 
 namespace InEditor
@@ -6,52 +7,52 @@ namespace InEditor
     {
         [InEditor(0)]
         public static bool b;
-
+        
         [InEditor(1)]
         public static int i;
-
+        
         [InEditor(2)]
         public static float f;
-
+        
         [InEditor(3)]
         public static string str;
-
+        
         [InEditor(4)]
         public static Rect r;
-
+        
         [InEditor(5)]
         public static RectInt ri;
-
+        
         [InEditor(6)]
         public static Color c;
-
+        
         [InEditor(7)]
         public static Color32 c32;
-
+        
         [InEditor(8)]
         public static Vector2 v2;
-
+        
         [InEditor(9)]
         public static Vector2Int v2I;
-
+        
         [InEditor(10)]
         public static Vector3 v3;
-
+        
         [InEditor(11)]
         public static Vector3Int v3I;
-
+        
         [InEditor(12)]
         public static Vector4 v4;
-
+        
         [InEditor(13)]
         public static LayerMask layerMask;
-
+        
         [InEditor(14)]
-        public static Gradient grad;
-
+        public static Gradient grad = new Gradient();
+        
         [InEditor(15)]
         public static AnimationCurve curve;
-
+        
         [InEditor(16)]
         public static GameObject obj;
     }
